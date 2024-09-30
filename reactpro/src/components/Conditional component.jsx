d04613c0941e1cd0e917cefe32b1d3d6f7cee280
thodoju <thodoju1@gmail.com>
2024-09-30 12:43:48 +0300

    using ternary operators in react

--- reactpro/src/components/Conditional component.jsx
+++ reactpro/src/components/Conditional component.jsx
@@ -2,12 +2,7 @@ import Code from "./Code";
 import Welcome from "./Welcome";
 
 export default function ConditionalComponent() {
-  let message;
-  const display = false;
-  if (display) {
-    message = <h1>Welcome to Code Everyday!</h1>;
-  } else {
-    message = <h1>Goodbye Code Everyday!</h1>;
-  }
+  const display = true;
+  return display ? <Welcome /> : <Code />;
   return message;
 }
