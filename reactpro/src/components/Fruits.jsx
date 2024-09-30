6c0569eb8d0b27dd63cd3dcd9ca097dc95b5eb9d
thodoju <thodoju1@gmail.com>
2024-09-30 09:46:44 +0300

    fruits.jsx

--- reactpro/src/components/Fruits.jsx
+++ reactpro/src/components/Fruits.jsx
@@ -1,4 +1,44 @@
 export default function Fruits() {
-  const fruits = ["Apple", "Mango", "Banana", "orange", "Pineapple"];
-  return <div>{fruits}</div>;
+  //const fruits = ["Apple", "Mango", "Banana", "orange", "Pineapple"];
+
+  const fruits = [
+    {
+      name: " Apple",
+      price: 10,
+      emoji: "🍎",
+    },
+    {
+      name: " Mango",
+      price: 7,
+      emoji: "🥭",
+    },
+    {
+      name: " Banana",
+      price: 2,
+      emoji: "🍌",
+    },
+    {
+      name: " Orange",
+      price: 5,
+      emoji: "🍊",
+    },
+    {
+      name: " Pineapple",
+      price: 8,
+      emoji: "🍍",
+    },
+  ];
+
+  return (
+    <div>
+      <ul>
+        {fruits.map((fruit) => (
+          <li key={fruit.name}>
+            {" "}
+            {fruit.emoji} {fruit.name} {fruit.price}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
