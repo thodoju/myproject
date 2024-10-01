bee6db75c94f107bf230142465a8936b59da12ad
thodoju <thodoju1@gmail.com>
2024-10-01 10:38:16 +0300

    Handling Input Fields In React

--- reactpro/src/components/Form.jsx
+++ reactpro/src/components/Form.jsx
@@ -1,18 +1,29 @@
 import { useState } from "react";
 
 export default function Form() {
-  const [name, setName] = useState("");
+  const [name, setName] = useState({ firstName: "", lastName: "" });
   function handleChange(e) {
     setName(e.target.value);
+  }
+  function handleSubmit(e) {
+    e.preventdefault();
+    console.log(name);
   }
   return (
     <div>
       <form>
         <input
-          onChange={(e) => setName(e.target.value)}
+          onChange={(e) => setName({ ...name, firstName: e.target.value })}
           type="text"
-          value={name}
+          value={name.firstName}
         />
+
+        <input
+          onChange={(e) => setName({ ...name, lastName: e.target.value })}
+          type="text"
+          value={name.lastName}
+        />
+        <button onClick={(e) => handleSubmit(e)}>Add</button>
       </form>
     </div>
   );
